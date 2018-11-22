415eabd1451ceee2d604117a78129b02897f1a65
Djoe_ <mail@djoe.ovh>
2018-11-22 07:57:23 +0100

    add scriphookv3 (modified with appi patch) & NativeUI v3

---
+++ scripthookvdotnet-dev_v3/source/core/CallCollection.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+namespace GTA.Native
+{
+    public class CallCollection
+    {
+        private List<Function.NativeTask> taskList;
+
+        public CallCollection()
+        {
+            taskList = new List<Function.NativeTask>();
+        }
+
+        public void Call(Hash hash, params InputArgument[] args)
+        {
+            var task = new Function.NativeTask();
+            task.Hash = (ulong)hash;
+            task.Arguments = args;
+
+            taskList.Add(task);
+        }
+
+        public int Execute()
+        {
+            var taskColl = new Function.NativeTaskCollection();
+            taskColl._tasks = new Function.NativeTask[taskList.Count];
+
+            for (int i = 0; i < taskList.Count; i++)
+            {
+                taskColl._tasks[i] = taskList[i];
+            }
+
+            ScriptDomain.CurrentDomain.ExecuteTask(taskColl);
+            return taskList.Count;
+        }
+    }
+}
